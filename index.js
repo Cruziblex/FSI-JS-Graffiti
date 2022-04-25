59aea4f47f879d73cdcb3fc36f18cfa03eeb8bbb
Alejandro Cruz <dragex@yahoo.com>
2022-04-25 18:39:53 -0400

    added background and image styling using JS

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let header = document.querySelector('#page-header')
-header.style.textAlign = "left"
+header.style.color = "navy"
 
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
 	dogImages[i].style.borderRadius = '50px'
 }
 
+let row = document.querySelector('.row')
+row.style.backgroundColor = "darkgray"
 
+let mid = document.querySelector('#mid')
+mid.style.backgroundColor = "lightgray"
 // Select some elements...
